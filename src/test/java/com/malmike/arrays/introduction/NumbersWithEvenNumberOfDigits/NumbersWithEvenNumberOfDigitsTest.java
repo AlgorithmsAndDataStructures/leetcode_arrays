2b4c29353886a8eb2b8941dfcdf08e0b9873bea6
Michael Male <malmike21@gmail.com>
2020-11-23 15:57:31 +0300

    set up numbers with even numbers

---
+++ src/test/java/com/malmike/arrays/introduction/NumbersWithEvenNumberOfDigits/NumbersWithEvenNumberOfDigitsTest.java
@@ -0,0 +1,15 @@
+package com.malmike.arrays.introduction.NumbersWithEvenNumberOfDigits;
+
+import junit.framework.TestCase;
+
+public class NumbersWithEvenNumberOfDigitsTest extends TestCase{
+    private NumbersWithEvenNumberOfDigits numbersWithEvenNumberOfDigits;
+
+    public NumbersWithEvenNumberOfDigitsTest(){
+        numbersWithEvenNumberOfDigits = new NumbersWithEvenNumberOfDigits();
+    }
+
+    public void testFindNumbers() {
+        assertTrue(true);
+    }
+}
