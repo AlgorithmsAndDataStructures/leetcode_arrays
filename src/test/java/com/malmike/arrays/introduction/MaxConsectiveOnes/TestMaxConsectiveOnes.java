5b762f8a18ca41f5d5e66e96c1f5520557441c8c
Michael Male <malmike21@gmail.com>
2020-11-24 14:52:17 +0300

    change to using jupiter test suite

--- src/test/java/com/malmike/arrays/introduction/MaxConsectiveOnes/TestMaxConsectiveOnes.java
+++ src/test/java/com/malmike/arrays/introduction/MaxConsectiveOnes/TestMaxConsectiveOnes.java
@@ -1,21 +1,21 @@
 package com.malmike.arrays.introduction.MaxConsectiveOnes;
 
-import junit.framework.TestCase;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MaxConsectiveOnesTest extends TestCase {
-    private MaxConsectiveOnes maxConsectiveOnes;
+import org.junit.jupiter.api.Test;
 
-    public MaxConsectiveOnesTest() {
-        maxConsectiveOnes = new MaxConsectiveOnes();
-    }
-
+public class TestMaxConsectiveOnes {
+    @Test
     public void testFindMaxConsectiveOnes() {
+        MaxConsectiveOnes maxConsectiveOnes = new MaxConsectiveOnes();
         int[] nums = new int[] { 1, 1, 0, 1, 1, 1 };
         int maxValue = maxConsectiveOnes.findMaxConsectiveOnes(nums);
         assertEquals(maxValue, 3);
     }
 
+    @Test
     public void testArrayContainsOnlyZeors() {
+        MaxConsectiveOnes maxConsectiveOnes = new MaxConsectiveOnes();
         int[] nums = new int[] { 0, 0, 0, 0, 0 };
         int maxValue = maxConsectiveOnes.findMaxConsectiveOnes(nums);
         assertEquals(maxValue, 0);
