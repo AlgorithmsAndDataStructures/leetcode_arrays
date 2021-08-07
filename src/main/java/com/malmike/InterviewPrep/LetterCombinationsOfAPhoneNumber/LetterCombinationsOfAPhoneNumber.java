ab809428df285387978158a7da0c5de301f1e410
Male Michael <malmike21@gmail.com>
2021-08-08 00:39:06 +0300

    Range sum query 2d (#14)
    * letter combinations of a phone number
    
    * remove unused packages

---
+++ src/main/java/com/malmike/InterviewPrep/LetterCombinationsOfAPhoneNumber/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,62 @@
+package com.malmike.InterviewPrep.LetterCombinationsOfAPhoneNumber;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class LetterCombinationsOfAPhoneNumber {
+    Map<Character, String[]> digitsMap = new HashMap<Character, String[]>() {
+        {
+            put('2', new String[] { "a", "b", "c" });
+            put('3', new String[] { "d", "e", "f" });
+            put('4', new String[] { "g", "h", "i" });
+            put('5', new String[] { "j", "k", "l" });
+            put('6', new String[] { "m", "n", "o" });
+            put('7', new String[] { "p", "q", "r", "s" });
+            put('8', new String[] { "t", "u", "v" });
+            put('9', new String[] { "w", "x", "y", "z" });
+        }
+    };
+
+    List<String> result2;
+
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<String>();
+        for (int i = 0; i < digits.length(); i++) {
+            char num = digits.charAt(i);
+            if (i == 0) {
+                result = Arrays.asList(digitsMap.get(num));
+            } else {
+                String[] values = digitsMap.get(num);
+                List<String> resp = new ArrayList<String>();
+                for (String x : result) {
+                    for (String letter : values) {
+                        resp.add(x + letter);
+                    }
+                }
+                result = resp;
+            }
+        }
+        return result;
+    }
+
+    public List<String> letterCombinationsBackTracking(String digits) {
+        this.result2 = new ArrayList<String>();
+        if (digits.length() > 0)
+            this.letterCombinationsRecursive("", digits, 0);
+        return this.result2;
+    }
+
+    private void letterCombinationsRecursive(String currentState, String digits, int position) {
+        if (position >= digits.length()) {
+            this.result2.add(currentState);
+            return;
+        }
+        String[] charValues = digitsMap.get(digits.charAt(position));
+        for (String value : charValues) {
+            this.letterCombinationsRecursive(currentState + value, digits, position + 1);
+        }
+    }
+}
---
+++ src/test/java/com/malmike/InterviewPrep/LetterCombinationsOfAPhoneNumber/TestLetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,65 @@
+package com.malmike.InterviewPrep.LetterCombinationsOfAPhoneNumber;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.Test;
+
+public class TestLetterCombinationsOfAPhoneNumber {
+    private LetterCombinationsOfAPhoneNumber letterCombinationsOfAPhoneNumber;
+
+    public TestLetterCombinationsOfAPhoneNumber() {
+        letterCombinationsOfAPhoneNumber = new LetterCombinationsOfAPhoneNumber();
+    }
+
+    @Test
+    public void testCase1() {
+        String input = "23";
+        List<String> expected = Arrays.asList("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf");
+        List<String> output = this.letterCombinationsOfAPhoneNumber.letterCombinations(input);
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void testCase2() {
+        String input = "";
+        List<String> expected = Arrays.asList();
+        List<String> output = this.letterCombinationsOfAPhoneNumber.letterCombinations(input);
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void testCase3() {
+        String input = "2";
+        List<String> expected = Arrays.asList("a", "b", "c");
+        List<String> output = this.letterCombinationsOfAPhoneNumber.letterCombinations(input);
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void testCase4() {
+        String input = "23";
+        List<String> expected = Arrays.asList("ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf");
+        List<String> output = this.letterCombinationsOfAPhoneNumber.letterCombinationsBackTracking(input);
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void testCase5() {
+        String input = "";
+        List<String> expected = new ArrayList<String>();
+        List<String> output = this.letterCombinationsOfAPhoneNumber.letterCombinationsBackTracking(input);
+        assertEquals(expected, output);
+    }
+
+    @Test
+    public void testCase6() {
+        String input = "2";
+        List<String> expected = Arrays.asList("a", "b", "c");
+        List<String> output = this.letterCombinationsOfAPhoneNumber.letterCombinationsBackTracking(input);
+        assertEquals(expected, output);
+    }
+}
